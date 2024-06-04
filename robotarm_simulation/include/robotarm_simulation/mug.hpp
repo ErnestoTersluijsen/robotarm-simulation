bc736c84460c3c657eeba2e66517ed54a5736b24
Ernesto Tersluijsen <EAW.Tersluijsen@student.han.nl>
2024-06-04 11:41:46 +0200

    more doxygen

--- robotarm_simulation/include/robotarm_simulation/mug.hpp
+++ robotarm_simulation/include/robotarm_simulation/mug.hpp
@@ -8,34 +8,93 @@
 #include "tf2_ros/transform_listener.h"
 #include <rclcpp/rclcpp.hpp>
 
+/**
+ * @brief Node updating the position of the mug depending on gravity and if it's picked up.
+ *
+ */
 class Mug : public rclcpp::Node
 {
   public:
+	/**
+	 * @brief Construct a new Mug object
+	 *
+	 */
 	Mug();
 
   private:
+	/**
+	 * @brief Updates the position of the mug to the simulation.
+	 *
+	 */
 	void update_mug();
 
+	/**
+	 * @brief Calculates the distance between 2 points in 3d space.
+	 * 
+	 * @param p1 Point 1.
+	 * @param p2 Point 2.
+	 * @return double Distance between points.
+	 */
 	double calculate_distance(const geometry_msgs::msg::Vector3& p1, const geometry_msgs::msg::Vector3& p2) const;
 
+	/**
+	 * @brief Function for applying gravity to the position of the mug.
+	 * 
+	 */
 	void update_gravity();
 
+	/**
+	 * @brief Timer for updating the position of the mug.
+	 * 
+	 */
 	rclcpp::TimerBase::SharedPtr timer_;
 
+	/**
+	 * @brief Broadcaster to publish the position of the mug.
+	 * 
+	 */
 	std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
+	/**
+	 * @brief Message containing the position of the mug.
+	 * 
+	 */
 	geometry_msgs::msg::TransformStamped msg_;
 
+	/**
+	 * @brief Listener to receive the position of the robotarm.
+	 * 
+	 */
 	std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
+	/**
+	 * @brief Buffer for the tf_listener_.
+	 * 
+	 */
 	std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 
+	/**
+	 * @brief Contains the previous position of the hand of the robotarm.
+	 * 
+	 */
 	geometry_msgs::msg::TransformStamped previous_hand_;
 
+	/**
+	 * @brief Amount of time between updates
+	 * 
+	 */
 	unsigned long update_interval_;
 
+	/**
+	 * @brief Current velocity downward of the mug.
+	 * 
+	 */
 	double current_velocity_;
 
+	/**
+	 * @brief Offset for the mug so the center of the mug is still in the middle and it doesn't phase through the ground.
+	 * 
+	 */
 	double mug_height_offset_;
 };
 
--- robotarm_simulation/include/robotarm_simulation/robotarm_simulation.hpp
+++ robotarm_simulation/include/robotarm_simulation/robotarm_simulation.hpp
@@ -5,9 +5,17 @@
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <std_msgs/msg/string.hpp>
 
+/**
+ * @brief Node for publishing the position of the robotarm and receiving commands.
+ *
+ */
 class RobotarmSimulation : public rclcpp::Node
 {
   public:
+	/**
+	 * @brief Construct a new Robotarm Simulation object.
+	 *
+	 */
 	RobotarmSimulation();
 
   private:
@@ -26,7 +34,7 @@ class RobotarmSimulation : public rclcpp::Node
 
 	/**
 	 * @brief Function to convert pwm value to radians.
-	 * 
+	 *
 	 * @param pwm Pulse width for the servo.
 	 * @return double Converted value in radians.
 	 */
@@ -34,7 +42,7 @@ class RobotarmSimulation : public rclcpp::Node
 
 	/**
 	 * @brief Function to convert pwm value to meters for the gripper.
-	 * 
+	 *
 	 * @param pwm Pulse width for the servo.
 	 * @return double Converted value in meters.
 	 */
@@ -42,49 +50,49 @@ class RobotarmSimulation : public rclcpp::Node
 
 	/**
 	 * @brief Subscription to the topic on which the commands are posted.
-	 * 
+	 *
 	 */
 	rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 
 	/**
 	 * @brief Publisher publishing the positions of all the joints of the robotarm.
-	 * 
+	 *
 	 */
 	rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
 
 	/**
 	 * @brief Timer for sending updates to the simulation every couple of milliseconds.
-	 * 
+	 *
 	 */
 	rclcpp::TimerBase::SharedPtr timer_;
 
 	/**
 	 * @brief Message being send by the publisher containing the states of the joints.
-	 * 
+	 *
 	 */
 	sensor_msgs::msg::JointState msg_;
 
 	/**
 	 * @brief Position where all the servo's should move to. This is not the current position. The current position is contained inside the msg_ variable.
-	 * 
+	 *
 	 */
 	std::vector<double> positions_ = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
 	/**
 	 * @brief The amount of distance the current position should move every update.
-	 * 
+	 *
 	 */
 	std::vector<double> steps_ = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
 	/**
-	 * @brief Minimum time it takes to complete any movement. Limits the speed of the robotarm. 
-	 * 
+	 * @brief Minimum time it takes to complete any movement. Limits the speed of the robotarm.
+	 *
 	 */
 	unsigned long min_moving_time_;
 
 	/**
 	 * @brief Interval time between the robotarm joint state updates.
-	 * 
+	 *
 	 */
 	unsigned long update_interval_;
 };
